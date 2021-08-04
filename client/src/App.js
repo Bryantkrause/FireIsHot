5f2be7f04955b3e509609dfd1e2464074ca8d102
bryantkrause@gmail.com <bryantkrause@gmail.com>
2021-08-04 07:00:11 -0700

    Initial set up for FireisHot

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a>
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
